c4521d675c1452e2e79664ec3ead27b53166720f
BrentDietrich <bmddev@gmail.com>
2017-09-10 15:32:04 -0700

    fixed unit test fail

--- src/app/features/credit/credit-inquiry/credit-inquiry.component.spec.ts
+++ src/app/features/credit/credit-inquiry/credit-inquiry.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
+import { SharedModule } from '../../../shared/shared.module';
+import { MaterialLoadModule } from '../../../shared/material.module';
 import { CreditInquiryComponent } from './credit-inquiry.component';
 
 describe('CreditInquiryComponent', () => {
@@ -8,6 +14,13 @@ describe('CreditInquiryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        SharedModule,
+        MaterialLoadModule,
+        CovalentDynamicFormsModule,
+        RouterTestingModule,
+        NoopAnimationsModule,
+      ],
       declarations: [ CreditInquiryComponent ]
     })
     .compileComponents();
